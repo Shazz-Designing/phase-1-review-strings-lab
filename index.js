d551f21f293267a15e60c64c099591ad59a1b5a3
sharon <sapot@safaricom.co.ke>
2023-10-14 21:36:24 +0300

    Initial Commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// Define the currentUser variable
+const currentUser = 'Sharon Apot';
+
+// Define the welcomeMessage variable
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define the excitedWelcomeMessage variable
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Define the shortGreeting variable
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
